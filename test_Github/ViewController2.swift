30f625478c0f96a587217564a69c83386e0aa220
Juilee31 <98794063+Juilee31@users.noreply.github.com>
2022-05-31 13:22:04 +0530

    modified print statement

--- test_Github/ViewController2.swift
+++ test_Github/ViewController2.swift
@@ -13,7 +13,7 @@ class ViewController2: UIViewController {
         super.viewDidLoad()
         
         
-        print("Mithilesh kia bataya? ")
+        print("Mithilesh kia batay? ")
         // Do any additional setup after loading the view.
     }
     
